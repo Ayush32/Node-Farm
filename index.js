50e1961ed7e921727f92fed8137a4fe5fb7f00f7
Ayush Gupta <721ayush@gmail.com>
2020-07-28 00:25:28 +0530

    filling the templattes

--- index.js
+++ index.js
@@ -44,22 +44,57 @@ const url = require('url');
 // console.log('Will read file') 
 // synchronous code
 // api sending route
-const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, "utf-8");
+
+const replaceTemplate = (temp, product) => {
+    let output  = temp.replace(/{% PRODUCTNAME %}/g, product.productName)
+    output = output.replace(/{% IMAGE %}/g, product.image);
+    output = output.replace(/{% PRICE %}/g, product.price);
+    output = output.replace(/{% QUANTITY %}/g, product.quantity);
+    output = output.replace(/{% FROM %}/g, product.from);
+    output = output.replace(/{% NUTRIENTS %}/g, product.nutrients);
+    output = output.replace(/{% DESCRIPTION %}/g, product.description);
+    output = output.replace(/{% ID %}/g, product.id);
+
+    if(!product.organic)
+    output = output.replace(/{% NOT_ORGANIC %}/g, 'not-organic');
+    return output;
+}
+
+const tempOverview = fs.readFileSync(`${__dirname}/templates/template-overview.html`, "utf-8");
+const tempCard = fs.readFileSync(`${__dirname}/templates/template-card.html`, "utf-8");
+const tempProduct = fs.readFileSync(`${__dirname}/templates/template-product.html`, "utf-8");
+
+const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, 'utf-8')
 const dataObj = JSON.parse(data);
 // / /
 const server = http.createServer((req, res) => {
-    const pathName =  req.url;
-    if(pathName === '/' || pathName === '/overview'){
-        res.end('This is the overview')
+    const pathName = req.url;
+
+    // overview page
+    if (pathName === '/' || pathName === '/overview') {
+        res.writeHead(200, {
+            "Content-type": "text/html"
+        });
+
+        const cardsHtml = dataObj.map(element => replaceTemplate(tempCard, element)).join('');
+        const output = tempOverview.replace('{% PRODUCT_CARDS %}', cardsHtml);
+        // console.log(cardsHtml)
+        res.end(output)
     }
-    else if(pathName === '/product'){
+    // product page
+    else if (pathName === '/product') {
+
         res.end('This is the Products')
     }
-    else if(pathName === '/api'){
-        res.writeHead(200, {'Content-type' : 'application/json'});
+    // API
+    else if (pathName === '/api') {
+        res.writeHead(200, {
+            'Content-type': 'application/json'
+        });
         res.end(data);
     }
-    else{
+    // Not found
+    else {
         res.writeHead(404, {
             'Content-type': 'text/html',
             'my-own-header': 'hello world'
@@ -71,6 +106,3 @@ const server = http.createServer((req, res) => {
 server.listen(8000, '127.0.0.1', () => {
     console.log('LIST TO REQUEST ON PORT 8000')
 });
-
-
-
