7fccf1aaca00793e7db1c450f7371041a4879ef2
Ayush Gupta <721ayush@gmail.com>
2020-07-28 00:47:43 +0530

    using modules: our own modules

--- index.js
+++ index.js
@@ -6,6 +6,7 @@
 const fs = require('fs');
 const http = require('http')
 const url = require('url');
+const replaceTemplate = require('./module/replaceTemplate')
 
 // asynchronous non blocking code
 // const fs = require('fs');
@@ -45,20 +46,6 @@ const url = require('url');
 // synchronous code
 // api sending route
 
-const replaceTemplate = (temp, product) => {
-    let output  = temp.replace(/{% PRODUCTNAME %}/g, product.productName)
-    output = output.replace(/{% IMAGE %}/g, product.image);
-    output = output.replace(/{% PRICE %}/g, product.price);
-    output = output.replace(/{% QUANTITY %}/g, product.quantity);
-    output = output.replace(/{% FROM %}/g, product.from);
-    output = output.replace(/{% NUTRIENTS %}/g, product.nutrients);
-    output = output.replace(/{% DESCRIPTION %}/g, product.description);
-    output = output.replace(/{% ID %}/g, product.id);
-
-    if(!product.organic)
-    output = output.replace(/{% NOT_ORGANIC %}/g, 'not-organic');
-    return output;
-}
 
 const tempOverview = fs.readFileSync(`${__dirname}/templates/template-overview.html`, "utf-8");
 const tempCard = fs.readFileSync(`${__dirname}/templates/template-card.html`, "utf-8");
