bb3c930071176efa0b9d69ee9a265d3db16a4cc2
Ayush Gupta <721ayush@gmail.com>
2020-07-28 00:39:45 +0530

    parsing  variables from url

--- index.js
+++ index.js
@@ -68,10 +68,11 @@ const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, 'utf-8')
 const dataObj = JSON.parse(data);
 // / /
 const server = http.createServer((req, res) => {
-    const pathName = req.url;
+    const { query, pathname } = url.parse(req.url, true);
+    
 
     // overview page
-    if (pathName === '/' || pathName === '/overview') {
+    if (pathname === '/' || pathname === '/overview') {
         res.writeHead(200, {
             "Content-type": "text/html"
         });
@@ -82,12 +83,16 @@ const server = http.createServer((req, res) => {
         res.end(output)
     }
     // product page
-    else if (pathName === '/product') {
-
-        res.end('This is the Products')
+    else if (pathname === '/product') {
+        res.writeHead(200, {
+          "Content-type": "text/html",
+        });
+       const product = dataObj[query.id]
+       const output = replaceTemplate(tempProduct, product)
+        res.end(output)
     }
     // API
-    else if (pathName === '/api') {
+    else if (pathname === '/api') {
         res.writeHead(200, {
             'Content-type': 'application/json'
         });
